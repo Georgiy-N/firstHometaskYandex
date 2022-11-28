f7a69c0835b6208d112878112de75753de1a71a9
Georgiy-N <neguritsageorgiy@gmail.com>
2022-11-28 15:25:44 +0600

    Для проверки домашнего задания

--- CounterTest/ViewController.swift
+++ CounterTest/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterTextLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var counter = 0
+    @IBAction func tapOnButton(_ sender: Any) {
+        counter = counter + 1
+        counterTextLabel.text = "Значение счетчика : \(counter)"
+        
+    }
+    
 }
 
